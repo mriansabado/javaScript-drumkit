d5e9b9af3c3f542499776a9c76a917d81c439c6c
Ian <isabado@onpointglobal.com>
2024-07-27 17:37:05 -0700

    updated js to play on down click

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ function removeTransition(e) {
 }
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode || e.target.dataset.key}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode || e.target.dataset.key}"]`);
+    const keyCode = e.keyCode || e.target.closest('.key').dataset.key;
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
@@ -24,7 +25,13 @@ function playSoundOnClick(e) {
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => {
     key.addEventListener('transitionend', removeTransition);
-    key.addEventListener('click', playSoundOnClick);
+    key.addEventListener('pointerdown', playSoundOnClick); // Use pointerdown for faster response on touch devices
+});
+
+// Preload all audio elements
+const audioElements = document.querySelectorAll('audio');
+audioElements.forEach(audio => {
+    audio.load();
 });
 
 window.addEventListener('keydown', playSound);
