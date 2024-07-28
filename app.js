504bf5bc68d11cf59b17f1912f5c92b0d4863acd
Ian <isabado@onpointglobal.com>
2024-07-27 17:34:15 -0700

    updated the buttons to fire on mobile and mouse click

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-  }
+}
 
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode || e.target.dataset.key}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode || e.target.dataset.key}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-  }
+}
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+function playSoundOnClick(e) {
+    const key = e.target.closest('.key');
+    if (!key) return;
+    
+    const keyCode = key.dataset.key;
+    playSound({ keyCode });
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', playSoundOnClick);
+});
+
+window.addEventListener('keydown', playSound);
